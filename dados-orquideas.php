abc710f70882a6179a1c06370d39b8d0c147f034
José Victor da Silva <sjosevictor@gmail.com>
2025-06-03 20:46:29 -0300

    Finalizando a pagina de orquideas

--- dados-orquideas.php
+++ dados-orquideas.php
@@ -22,7 +22,7 @@
             'name' => 'Orquídea-Phalaenopsis',
             'scientific' => 'Phalaenopsis × singuliflora J.J.Sm.',
             'description' => 'As hastes florais podem se ramificar ou até florescer novamente se não forem cortadas após a primeira floração.',
-            'price' => 35.00,
+            'price' => 45.00,
             'whatsapp' => '5544998014519',
             'image' => 'imagens/orquidea4.png',
             'stock' => 12,
@@ -38,7 +38,7 @@
             'name' => 'Cattleya rosa',
             'scientific' => 'Cattleya trianae Linden',
             'description' => 'Com pétalas e sépalas em tons de rosa a lilás claro, geralmente com um labelo mais escuro e intenso, frequentemente com tons de amarelo ou dourado no centro..',
-            'price' => 35.00,
+            'price' => 65.00,
             'whatsapp' => '5544998014519',
             'image' => 'imagens/orquidea5.png',
             'stock' => 8,
@@ -54,7 +54,7 @@
             'name' => 'Olho-de-boneca ',
             'scientific' => 'Dendrobium bigibbum Lindl.',
             'description' => 'Pode produzir várias flores em uma única haste, geralmente em racemos curvos ou eretos.',
-            'price' => 35.00,
+            'price' => 30.00,
             'whatsapp' => '5544998014519',
             'image' => 'imagens/orquidea6.png',
             'stock' => 8,
@@ -67,97 +67,97 @@
         ],
         [
             'id' => 5,
-            'name' => 'Almofada da sogra',
-            'scientific' => 'Echinocactus grusonii',
-            'description' => 'Possui formato globular ou levemente achatado, com cor verde-vivo e costelas bem definidas, cobertas por espinhos longos, retos e dourados.',
-            'price' => 35.00,
+            'name' => 'Cymbidium',
+            'scientific' => 'Cymbidium devonianum Paxton',
+            'description' => 'É uma espécie apreciada por suas flores elegantes, longas hastes florais e folhagem atrativa.',
+            'price' => 55.00,
             'whatsapp' => '5544998014519',
-            'image' => 'imagens/cacto8.png',
+            'image' => 'imagens/orquidea7.png',
             'stock' => 3,
             'care' => [
-                'light' => 'Sol pleno',
+                'light' => 'Meia-sombra',
                 'water' => 'Pouca',
-                'temp' => '20-30°C'
+                'temp' => '10-25°C'
             ],
 
         ],
         [
             'id' => 6,
-            'name' => 'Cacto cabeça-de-frade',
-            'scientific' => 'Gymnocalycium mihanovichii',
-            'description' => 'cacto globular com corpo verde-escuro, cinza-esverdeado ou até roxo, e sem costelas muito marcadas — possui sulcos suaves e espinhos curtos e finos.',
-            'price' => 35.00,
+            'name' => 'Cimbídio',
+            'scientific' => 'Cymbidium devonianum Paxton',
+            'description' => 'Muito valorizada em coleções por sua floração abundante e longa durabilidade das flores.',
+            'price' => 30.00,
             'whatsapp' => '5544998014519',
-            'image' => 'imagens/cacto9.png',
+            'image' => 'imagens/orquidea8.png',
             'stock' => 8,
             'care' => [
                 'light' => 'Meia-sombra',
-                'water' => 'Moderada',
-                'temp' => '18-25°C'
+                'water' => 'Pouca',
+                'temp' => '10-25°C'
             ],
 
         ],
         [
             'id' => 7,
-            'name' => 'Orelha do Mickey',
-            'scientific' => 'Opuntia microdasys',
-            'description' => 'Cacto ornamental com cladódios achatados em forma de "orelhas", geralmente dispostos aos pares.',
+            'name' => 'Orquidea Aranha',
+            'scientific' => 'Brassia arachnoidea',
+            'description' => 'Conhecida por suas flores muito peculiares que imitam a forma e o movimento de aranhas, uma adaptação para atrair polinizadores específicos como vespas.',
             'price' => 35.00,
             'whatsapp' => '5544998014519',
-            'image' => 'imagens/cacto10.png',
-            'stock' => 5,
+            'image' => 'imagens/orquidea9.png',
+            'stock' => 2,
             'care' => [
-                'light' => 'Sol pleno',
-                'water' => 'Pouca',
-                'temp' => '20-30°C'
-            ],
-
-        ],
-        [
-            'id' => 8,
-            'name' => 'Mandacaru azul',
-            'scientific' => 'Pilosocereus pachycladus F.Ritter',
-            'description' => 'Apresenta uma textura aveludada nas regiões de crescimento mais jovem e pode atingir vários metros de altura com o tempo.',
-            'price' => 35.00,
-            'whatsapp' => '5544998014519',
-            'image' => 'imagens/cacto11.png',
-            'stock' => 6,
-            'care' => [
-                'light' => 'Sol pleno',
-                'water' => 'Rega espaçada',
-                'temp' => '20-30°C'
-            ],
-
-        ],
-        [
-            'id' => 9,
-            'name' => 'Cacto bola de ouro',
-            'scientific' => 'Parodia lenninghausii',
-            'description' => 'Cacto ilíndrico e ereto, com corpo verde-claro coberto por espinhos finos e dourados, que lhe dão um visual suave e aveludado.',
-            'price' => 35.00,
-            'whatsapp' => '5544998014519',
-            'image' => 'imagens/cacto12.png',
-            'stock' => 0,
-            'care' => [
-                'light' => 'Sol pleno',
+                'light' => 'Meia-sombra',
                 'water' => 'Moderada',
                 'temp' => '18-28°C'
             ],
 
         ],
         [
-            'id' => 10,
-            'name' => 'Cacto bola',
-            'scientific' => 'Kroenleinia grusonii',
-            'description' => 'Cacto globular com linda coroa vermelha. Muito resistente e ornamental, perfeito para iniciantes.',
+            'id' => 8,
+            'name' => 'Orquídea borboleta',
+            'scientific' => 'Phalaenopsis amabilis (L.) Blume',
+            'description' => 'É uma das orquídeas mais vendidas no mundo por sua beleza, durabilidade e facilidade de cultivo.',
             'price' => 35.00,
             'whatsapp' => '5544998014519',
-            'image' => 'imagens/cacto13.png',
-            'stock' => 1,
+            'image' => 'imagens/orquidea10.png',
+            'stock' => 6,
             'care' => [
-                'light' => 'Sol pleno',
-                'water' => 'Pouca',
+                'light' => 'Meia-sombra',
+                'water' => 'Moderada',
                 'temp' => '20-30°C'
+            ],
+
+        ],
+        [
+            'id' => 9,
+            'name' => 'Orquídea borboleta',
+            'scientific' => 'Phalaenopsis amabilis (L.) Blume',
+            'description' => 'Muito usada em decoração de interiores e presente em buquês e arranjos sofisticados.',
+            'price' => 40.00,
+            'whatsapp' => '5544998014519',
+            'image' => 'imagens/orquidea11.png',
+            'stock' => 4,
+            'care' => [
+                'light' => 'Meia-sombra',
+                'water' => 'Moderada',
+                'temp' => '20-30°C'
+            ],
+
+        ],
+        [
+            'id' => 10,
+            'name' => 'Orquídea-zigopétala',
+            'scientific' => 'Zygopetalum maculatum (Kunth) Garay',
+            'description' => 'É uma orquídea nativa da América do Sul, especialmente encontrada nos andes do Equador, Peru e Colômbia, crescendo em regiões úmidas e sombreadas.',
+            'price' => 90.00,
+            'whatsapp' => '5544998014519',
+            'image' => 'imagens/orquidea12.png',
+            'stock' => 0,
+            'care' => [
+                'light' => 'Meia-sombra',
+                'water' => 'Moderada',
+                'temp' => '15-25°C'
             ],
 
         ]
